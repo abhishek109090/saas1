bab347105079aa9a6e9f0a1af1b848a2b6d3d2c9
abhi <19q91a05e8_cse@mrce.in>
2023-12-21 23:41:21 +0530

    deleted files

--- src/App.js
+++ src/App.js
@@ -1,52 +1,77 @@
-import React from 'react';
-import Header from './Header';
-import Navbar from './Navbar';
-import Login from './Login';
-import TruckOwner from './TruckOwner';
-import TruckVerification from './TruckVerification';
-import OwnerRegistration from './OwnerRegistration';
-import MainContent from './MainContent';
-import VerifiedTruck from './VerifiedTruck';
-import { BrowserRouter, Route, Routes,  useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import React from "react";
+import Dashboard from './Dashboard';
+import OwnerInterface from "./OwnerInterface";
+import AgentRegistration from "./AgentRegistration";
+import AgentInfo from "./AgentInfo";
+import NewTruck from "./NewTruck";
+import TruckPosting from "./TruckPosting";
+import OwnerBooking from "./OwnerBooking";
+import AgentNavbar from "./AgentNavbar";
+import AgentInterface from "./AgentInterface";
+import AgentBooking from "./AgentBooking";
+import AgentBooknow from "./AgentBooknow";
+import MyForm from "./MyForm";
+import AgentUpdate from "./AgentUpdate";
 
-function App() {
+import NewDriver from "./NewDriver";
+import DriverInfo from "./DriverInfo";
+import Footer from "./Footer";
+import NewPost from "./NewPost";
+import Calculator from "./Calculator";
+import AgentForm from "./AgentForm";
+
+// import CarouselWithCards from "./CarouselWithCards";
+// import VideoBackground from "./VideoBackground ";
+export default function App() {
+ 
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  );
-}
+    <div>
+      
 
-function AppRoutes() {
-  const navigate = useNavigate();
+     
+      <BrowserRouter >
+        <Routes>
+        <Route path='/' element={<Dashboard />}></Route>
 
-  const handleLogout = () => {
-    // Clear the user-related data from local storage
-    localStorage.removeItem('name');
-    localStorage.removeItem('userToken');
-    localStorage.removeItem('userCRN');
+          <Route path='Dashboard' element={<Dashboard />}></Route>
 
-    // Navigate to the login page
-    navigate('/');
-  };
+          <Route path='OwnerInterface' element={<OwnerInterface />}></Route>
+          <Route path='AgentRegistration' element={<AgentRegistration  />}></Route>
 
-  return (
-    <div style={{backgroundColor:'ghostwhite  ',width:'100%',height:'100vh'}}>
-      <Header onLogout={handleLogout} />
-      <Navbar />
-      <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path='AgentInfo' element={<AgentInfo  />}></Route>
+          <Route path='AgentInfo' element={<AgentInfo  />}></Route>
+          <Route path='NewDriver' element={<NewDriver  />}></Route>
+          <Route path='MyForm' element={<MyForm  />}></Route>
 
-          <Route path="/MainContent" element={<MainContent />} />
-          <Route path="/OwnerRegistration" element={<OwnerRegistration />} />
-          <Route path="/TruckVerification" element={<TruckVerification />} />
-          <Route path="/TruckOwner" element={<TruckOwner />} />
-          <Route path="/VerifiedTruck" element={<VerifiedTruck />} />
+          <Route path='NewTruck' element={<NewTruck  />}></Route>
+          <Route path='DriverInfo' element={<DriverInfo  />}></Route>
+          <Route path='AgentForm' element={<AgentForm  />}></Route>
 
-        
-      </Routes>
+          <Route path='TruckPosting' element={<TruckPosting  />}></Route>
+          <Route path='OwnerBooking' element={<OwnerBooking />}></Route>
+          <Route path='AgentNavbar' element={<AgentNavbar />}></Route>
+          <Route path='AgentUpdate' element={<AgentUpdate />}></Route>
+
+          <Route path='AgentInterface' element={<AgentInterface />}></Route>
+          <Route path='AgentBooking' element={<AgentBooking />}></Route>
+
+          <Route path='AgentBooknow' element={<AgentBooknow />}></Route>
+          
+
+          <Route path='newpost' element={<NewPost />}></Route>
+          <Route path='cal' element={<Calculator />}></Route>
+
+
+
+
+
+        </Routes>
+      </BrowserRouter>
+      {/* <CarouselWithCards/> */}
+      <Footer/>
+
     </div>
-  );
+  )
 }
-
-export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,60 +1,55 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import "jquery";
-import "bootstrap/dist/js/bootstrap.js";
-import "popper.js/dist/umd/popper";
-import "bootstrap/dist/css/bootstrap.css";
-
+import React from 'react'
+import './Navbar.css';
+import { Link, matchRoutes, useNavigate } from 'react-router-dom';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 export default function Navbar() {
-  const feildcrn = sessionStorage.getItem("userCRN");
-  const phonenumber = sessionStorage.getItem("phoneNumber");
-  const navigate = useNavigate();
 
-  const handleRegistration = () => {
-    navigate("/OwnerRegistration", { state: { feildcrn: feildcrn, phonenumber: phonenumber } });
+  const navigate = useNavigate(); 
+  const handleLogout = () => {
+    // Clear local storage and navigate to the login page
+    localStorage.removeItem('userCRN');
+    sessionStorage.removeItem('phoneNumber');
+    sessionStorage.removeItem('password');
+    navigate('/login'); // Navigate to the login page
   };
-  
-  const handleVerification = () => {
-    navigate("/TruckVerification", { state: { feildcrn: feildcrn, phonenumber: phonenumber } });
-  };
-  
-  const TruckOwner = () => {
-    navigate("/TruckOwner", { state: { feildcrn: feildcrn, phonenumber: phonenumber } });
-  };
-  const TruckVerified = () => {
-    navigate("/VerifiedTruck", { state: { feildcrn: feildcrn, phonenumber: phonenumber } });
-  };
-  const handleHome = () => {
-    navigate("/MainContent", { state: { feildcrn: feildcrn, phonenumber: phonenumber } });
-  };
-  return (
-    <nav className="navbar navbar-expand-md navbar-white bg-dark ms-2 me-2">
-      <div className="container-fluid">
-        <ul className="navbar-nav me-auto">
-          <li className="nav-item">
-          <button className="nav-link text-info" onClick={handleHome}>Home</button>
+  return ( 
+    
+    <div>
+<nav class="navbar navbar-expand-lg ">
+  <a class="navbar-brand" href="#">
+
+         <a href="/AddSub" style={{textDecoration:'none',color:'white'}} className='homeinnav'>
+    
+Home</a>
+  </a>
+  <button class="navbar-toggler bg-primary p-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <MenuOutlinedIcon fontSize='large' />
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+
+    
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+      <ul className="navbar-list" style={{fontWeight:'bold',color:'white'}}>
+          <li className="navbar-ite dropdown">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+  <path fill-rule="evenodd"  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+</svg>   My Profile
+            <div className="dropdown-content"style={{textDecoration:'none'}}>
+              <a href="#"style={{textDecoration:'none'}}>My Profile</a>
+              <a href="OwnerInterface" style={{textDecoration:'none'}}>Home</a>
+              <a href="Dashboard" onClick={handleLogout}style={{textDecoration:'none'}}>Logout</a>
+            </div>
           </li>
         </ul>
-        <button className="navbar-toggler text-white bg-info" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="mynavbar">
-          <ul className="navbar-nav me-auto ms-3 me-3">
-            <li className="nav-item">
-              <button className="nav-link text-info" onClick={handleRegistration}>Owner Registration</button>
-            </li>
-            <li className="nav-item">
-              <button className="nav-link text-info" onClick={TruckOwner}>My Truck Owners</button>
-            </li>
-            <li className="nav-item">
-              <button className="nav-link text-info" onClick={handleVerification}>Truck Verification</button>
-            </li>
-            <li className="nav-item">
-            <button className="nav-link text-info" onClick={TruckVerified}>Trucks Verified</button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+      </li>
+    </ul>
+  </div>
+</nav>
+      
+       
+       
+    </div>
+  )
 }
